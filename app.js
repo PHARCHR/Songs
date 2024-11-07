a00f3e1d03ba8dfd39a55e21e673a39783ef338b
PHARCHR <fargeta92@gmail.com>
2024-11-07 19:46:38 +0300

    minor correction

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const express = require("express");
 const home=require("./middlewares/homePage")
 const app = express();
 
-app.use('/',home)
+app.use(home)
 app.use(express.json());
 app.use('/api/v1',router)
 
